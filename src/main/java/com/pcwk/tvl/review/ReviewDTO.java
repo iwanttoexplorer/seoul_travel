4846297d49d9a653bb9a343358561094cc9b34da
김승준 <167057839+tmdwns520@users.noreply.github.com>
2024-06-13 17:22:51 +0900

    Add files via upload

--- src/main/java/com/pcwk/tvl/review/ReviewDTO.java
+++ src/main/java/com/pcwk/tvl/review/ReviewDTO.java
@@ -1,80 +1,120 @@
-package com.pcwk.tvl.review;
+package com.pcwk.ehr.travel;
 
-public class ReviewDTO {
-    private String aboardSeq; // 리뷰 순서번호
-    private String contentId; // 콘텐츠ID
-    private String userId; // 리뷰 작성자
-    private String imgLink; // 사진
-    private String comments; // 리뷰 내용
-    private String title; // 제목
-    private String regDt; // 작성일
-    private String modDt; // 수정일
-    public ReviewDTO() {}
-	@Override
-	public String toString() {
-		return "ReviewDTO [aboardSeq=" + aboardSeq + ", contentId=" + contentId + ", userId=" + userId + ", imgLink="
-				+ imgLink + ", comments=" + comments + ", title=" + title + ", regDt=" + regDt + ", modDt=" + modDt
-				+ "]";
+import java.util.Date;
+
+import com.pcwk.ehr.cmn.DTO;
+
+public class ReviewDTO extends DTO {
+    private int aboard_seq;
+    private String comment;
+    private String imgLink;
+    private String contentId;
+    private String user_id;
+    private String title;
+    private int readCnt;
+    private Date regDt;   
+    private Date modDt;
+    
+ // Getters and Setters
+    
+    public ReviewDTO() {
+    	
+    }
+    
+	public int getReadCnt() {
+		return readCnt;
 	}
-	public String getAboardSeq() {
-		return aboardSeq;
+	
+	public void setReadCnt(int readCnt) {
+		this.readCnt = readCnt;
 	}
-	public void setAboardSeq(String aboardSeq) {
-		this.aboardSeq = aboardSeq;
-	}
-	public String getContentId() {
-		return contentId;
-	}
-	public void setContentId(String contentId) {
-		this.contentId = contentId;
-	}
-	public String getUserId() {
-		return userId;
-	}
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	public String getImgLink() {
-		return imgLink;
-	}
-	public void setImgLink(String imgLink) {
-		this.imgLink = imgLink;
-	}
-	public String getComments() {
-		return comments;
-	}
-	public void setComments(String comments) {
-		this.comments = comments;
-	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public String getRegDt() {
+
+	public int getAboard_seq() {
+		return aboard_seq;
+	}
+
+	public void setAboard_seq(int aboard_seq) {
+		this.aboard_seq = aboard_seq;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public String getImgLink() {
+		return imgLink;
+	}
+
+	public void setImgLink(String imgLink) {
+		this.imgLink = imgLink;
+	}
+
+	public String getContentId() {
+		return contentId;
+	}
+
+	public void setContentId(String contentId) {
+		this.contentId = contentId;
+	}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public Date getRegDt() {
 		return regDt;
 	}
-	public void setRegDt(String regDt) {
+
+	public void setRegDt(Date regDt) {
 		this.regDt = regDt;
 	}
-	public String getModDt() {
+
+	public Date getModDt() {
 		return modDt;
 	}
-	public void setModDt(String modDt) {
+
+	public void setModDt(Date modDt) {
 		this.modDt = modDt;
 	}
-	public ReviewDTO(String aboardSeq, String contentId, String userId, String imgLink, String comments, String title,
-			String regDt, String modDt) {
+	
+	public ReviewDTO(int aboard_seq, String comment, String imgLink, String contentId, String user_id, String title,
+			int readCnt, Date regDt, Date modDt) {
 		super();
-		this.aboardSeq = aboardSeq;
-		this.contentId = contentId;
-		this.userId = userId;
+		this.aboard_seq = aboard_seq;
+		this.comment = comment;
 		this.imgLink = imgLink;
-		this.comments = comments;
+		this.contentId = contentId;
+		this.user_id = user_id;
 		this.title = title;
+		this.readCnt = readCnt;
 		this.regDt = regDt;
 		this.modDt = modDt;
 	}
+
+	@Override
+	public String toString() {
+		return "ReviewDTO [aboard_seq=" + aboard_seq + ", comment=" + comment + ", imgLink=" + imgLink + ", contentId="
+				+ contentId + ", user_id=" + user_id + ", title=" + title + ", readCnt=" + readCnt + ", regDt=" + regDt
+				+ ", modDt=" + modDt + "]";
+	}
+
+}  
+	
     
-}
+    
