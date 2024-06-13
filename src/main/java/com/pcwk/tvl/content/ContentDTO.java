a75645aef87e22717b5d5b3f8e7cc765d3459e12
H.J.Park <oopoo1425@gmail.com>
2024-06-13 17:43:01 +0900

    contentDTO 수정

--- src/main/java/com/pcwk/tvl/content/ContentDTO.java
+++ src/main/java/com/pcwk/tvl/content/ContentDTO.java
@@ -1,69 +1,111 @@
 package com.pcwk.tvl.content;
 
 public class ContentDTO {
-  private String contentId;
-  private String category;
-  private String guCode;
-  private String tel;
-  private String addr;
-  private String img_link;
-  private String title;
-  private String reg_dt;
-  private String mod_dt;
-  public ContentDTO() {}
-public String getContentId() {
-	return contentId;
-}
-public void setContentId(String contentId) {
-	this.contentId = contentId;
-}
-public String getCategory() {
-	return category;
-}
-public void setCategory(String category) {
-	this.category = category;
-}
-public String getGuCode() {
-	return guCode;
-}
-public void setGuCode(String guCode) {
-	this.guCode = guCode;
-}
-public String getTel() {
-	return tel;
-}
-public void setTel(String tel) {
-	this.tel = tel;
-}
-public String getAddr() {
-	return addr;
-}
-public void setAddr(String addr) {
-	this.addr = addr;
-}
-public String getImg_link() {
-	return img_link;
-}
-public void setImg_link(String img_link) {
-	this.img_link = img_link;
-}
-public String getTitle() {
-	return title;
-}
-public void setTitle(String title) {
-	this.title = title;
-}
-public String getReg_dt() {
-	return reg_dt;
-}
-public void setReg_dt(String reg_dt) {
-	this.reg_dt = reg_dt;
-}
-public String getMod_dt() {
-	return mod_dt;
-}
-public void setMod_dt(String mod_dt) {
-	this.mod_dt = mod_dt;
-}
-  
+
+	private String contentId; //콘텐츠ID
+	private String category;  //카테고리
+	private String gucode;    //구코드
+	private String tel;       //전화번호
+	private String addr;      //주소
+	private String imgLink;   //이미지링크
+	private String title;     //제목
+	private String regDt;     //등록일
+	private String modDt;     //수정일
+	
+	public ContentDTO() {}
+
+	public ContentDTO(String contentId, String category, String gucode, String tel, String addr, String imgLink,
+			String title, String regDt, String modDt) {
+		super();
+		this.contentId = contentId;
+		this.category = category;
+		this.gucode = gucode;
+		this.tel = tel;
+		this.addr = addr;
+		this.imgLink = imgLink;
+		this.title = title;
+		this.regDt = regDt;
+		this.modDt = modDt;
+	}
+
+	public String getContentId() {
+		return contentId;
+	}
+
+	public void setContentId(String contentId) {
+		this.contentId = contentId;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getGucode() {
+		return gucode;
+	}
+
+	public void setGucode(String gucode) {
+		this.gucode = gucode;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String getImgLink() {
+		return imgLink;
+	}
+
+	public void setImgLink(String imgLink) {
+		this.imgLink = imgLink;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getRegDt() {
+		return regDt;
+	}
+
+	public void setRegDt(String regDt) {
+		this.regDt = regDt;
+	}
+
+	public String getModDt() {
+		return modDt;
+	}
+
+	public void setModDt(String modDt) {
+		this.modDt = modDt;
+	}
+
+	@Override
+	public String toString() {
+		return "ContentDTO [contentId=" + contentId + ", category=" + category + ", gucode=" + gucode + ", tel=" + tel
+				+ ", addr=" + addr + ", imgLink=" + imgLink + ", title=" + title + ", regDt=" + regDt + ", modDt="
+				+ modDt + ", toString()=" + super.toString() + "]";
+	}
+
+	
 }
