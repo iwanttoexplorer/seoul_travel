1b38b6d130650d9250367ef4cc18c9b4c81ad6b1
M.S. Jang <ms3814@naver.com>
2024-06-17 15:29:51 +0900

    dd

--- src/main/java/com/pcwk/tvl/user/UserController.java
+++ src/main/java/com/pcwk/tvl/user/UserController.java
@@ -1,9 +1,53 @@
 package com.pcwk.tvl.user;
 
-import com.pcwk.ehr.cmn.PLog;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class UserController implements PLog{
+import com.pcwk.ehr.cmn.ControllerV;
+import com.pcwk.ehr.cmn.JView;
+
+/**
+ * Servlet implementation class UserController
+ */
+@WebServlet("/UserController")
+public class UserController extends HttpServlet implements ControllerV{
+	private static final long serialVersionUID = 1L;
+       
+	private UserService service;
 	
 	
-	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	@Override
+	public JView doWork(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
