b363f79384e1b64f6b6c7698becec8602fe55124
HexSnack <hs15210607@gmail.com>
2024-06-17 14:47:26 +0900

    frontcontrollerV 생성

---
+++ src/main/java/com/pcwk/ehr/servlet/FrontcontrollerV.java
@@ -0,0 +1,68 @@
+package com.pcwk.ehr.servlet;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.pcwk.ehr.cmn.ControllerV;
+import com.pcwk.ehr.cmn.JView;
+import com.pcwk.ehr.cmn.PLog;
+import com.pcwk.test.ConnectController;
+import com.pcwk.test.CookieController;
+
+/**
+ * Servlet implementation class FrontcontrollerV
+ */
+@WebServlet(description = "servlet", urlPatterns = { "*.journey" })
+public class FrontcontrollerV extends HttpServlet implements PLog{
+	private static final long serialVersionUID = 1L;
+	private Map<String, ControllerV> controllerMap = new HashMap<String, ControllerV>();
+   
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FrontcontrollerV() {
+    	log.debug("FrontControllerV()");
+    	
+    	//controllerMap.put("/WEB02/board/board.do", new BoardController());
+    	controllerMap.put("/SEOUL_TRAVEL/connect/connect.do", new ConnectController());
+    	
+    	controllerMap.put("/SEOUL_TRAVEL/cookie/cookie.do", new CookieController());
+    	//controllerMap.put("/WEB02/login/login.do", new LoginController());
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String requestURI = request.getRequestURI();
+		log.debug("requestURI: {}",requestURI);
+		
+		ControllerV controller = controllerMap.get(requestURI);
+		log.debug("controller: {}",controller);
+		
+		//request 인코딩 처리:
+    	request.setCharacterEncoding("UTF-8");
+		
+		//controller가 null이면 404예외처리
+		if(null == controller) {
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			return;
+		}
+		
+		JView jview = controller.doWork(request, response);
+		
+		if(null != jview && jview.getViewName().length()>0) {
+		jview.render(request, response);
+		log.debug("jview: {}",jview);
+		}
+	}
+
+}
