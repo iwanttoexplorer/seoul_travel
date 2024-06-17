8eb4ac130413a8af84bfdda14354217c01ec79b8
HexSnack <hs15210607@gmail.com>
2024-06-17 12:39:55 +0900

    LikeDao LikeMain ¿ϼº ¹× °ËÅä ¿Ϸá

--- src/main/java/com/pcwk/tvl/like/LikeDao.java
+++ src/main/java/com/pcwk/tvl/like/LikeDao.java
@@ -1,29 +1,125 @@
 package com.pcwk.tvl.like;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 import com.pcwk.ehr.cmn.ConnectionMaker;
+import com.pcwk.ehr.cmn.DBUtill;
+import com.pcwk.ehr.cmn.DTO;
 import com.pcwk.ehr.cmn.PLog;
+import com.pcwk.ehr.cmn.WorkDiv;
 
-public class LikeDao implements PLog{
+public class LikeDao implements PLog,WorkDiv<LikeDTO>{
 	
 	private ConnectionMaker connectionMaker;
 	public LikeDao() {
 		connectionMaker=new ConnectionMaker();
 	}
 	
-	public int like(String userId, int aboardSeq, String crtDt) {
-		
+    public int doSave(LikeDTO param) {
+		int flag=0;
 		Connection conn = connectionMaker.getConnection();
-		String SQL="INSERT INTO v_like values(?,?,?)";
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(SQL);
-			pstmt.setString(1, userId);
-			pstmt.setInt(2, aboardSeq);
-			pstmt.setString(3, crtDt);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		return -1;
+        PreparedStatement pstmt = null;
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("INSERT INTO v_like (          \n");
+        sb.append("    user_id,                 \n");
+        sb.append("    aboard_seq,              \n");
+        sb.append("    crt_dt                   \n");
+        sb.append(") VALUES (                   \n");
+        sb.append("    ?,                       \n");
+        sb.append("    ?,                       \n");
+        sb.append("    SYSDATE                  \n");
+        sb.append(")                            \n");
+        
+        log.debug("1.sql:{}", sb.toString());
+        log.debug("2.conn:{}", conn);
+        log.debug("3.param:{}", param);
+
+        try {
+            // preparestatement << 사용 금지!!! 해킹가능성!!!
+            pstmt = conn.prepareCall(sb.toString());
+            log.debug("4.pstmt:{}", pstmt);
+            
+            // param 설정
+            pstmt.setString(1, param.getUserId());
+            pstmt.setInt(2, param.getAboardSeq());
+
+            // DML
+            flag = pstmt.executeUpdate();
+
+           
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtill.close(conn, pstmt);
+            log.debug("5.finally conn:{} pstmt:{}", conn, pstmt);
+        }
+
+        log.debug("6.flag:{}", flag);
+
+        return flag;
+    }
+	
+    public int doLike(int aboardSeq) {
+    	int likeCount=0;
+    	Connection conn = connectionMaker.getConnection();
+    	PreparedStatement pstmt = null;
+    	ResultSet rs = null;
+    	StringBuilder sb = new StringBuilder();
+    	sb.append("SELECT COUNT(*)               \n");
+        sb.append("FROM v_like                   \n");
+        sb.append("WHERE aboard_seq = ?          \n");
+        log.debug("1.sql:{}", sb.toString());
+        log.debug("2.conn:{}", conn);
+        log.debug("3.aboardSeq:{}", aboardSeq);
+        try {
+            pstmt = conn.prepareCall(sb.toString());
+            log.debug("4.pstmt:{}", pstmt);
+
+            // param 설정
+            pstmt.setInt(1, aboardSeq);
+
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                likeCount = rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtill.close(conn, pstmt, rs);
+            log.debug("5.finally conn:{} pstmt:{} rs:{}", conn, pstmt, rs);
+        }
+
+        log.debug("6.likeCount:{}", likeCount);
+
+        return likeCount;
+    }
+	@Override
+	public List<LikeDTO> doRetrieve(DTO search) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+	@Override
+	public int doUpdate(LikeDTO param) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int doDelete(LikeDTO param) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public LikeDTO doSelectOne(LikeDTO param) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
---
+++ src/main/java/com/pcwk/tvl/like/LikeMain.java
@@ -0,0 +1,32 @@
+package com.pcwk.tvl.like;
+
+import com.pcwk.ehr.cmn.PLog;
+
+public class LikeMain implements PLog{
+	LikeDao dao;
+	LikeDTO like;
+	public LikeMain() {
+		dao = new LikeDao();
+		like = new LikeDTO("4kqD945", 1);
+	}
+	public void doSave() {
+		log.debug("doSave()");
+		int flag = dao.doSave(like);
+		if(1==flag) {
+			log.debug("저장 성공 :{}",flag);
+		}else {
+			log.debug("저장 실패 :{}",flag);
+		}
+	}
+	public void doLike() {
+		log.debug("doLike()");
+		int likeCount = dao.doLike(like.getAboardSeq());
+		log.debug("추천수: {}",likeCount);
+	}
+	public static void main(String[] args) {
+		LikeMain m = new LikeMain();
+//		m.doSave();
+		m.doLike();
+	}
+
+}
