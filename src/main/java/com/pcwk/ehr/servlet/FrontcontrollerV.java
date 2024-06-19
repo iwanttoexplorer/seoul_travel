f4dc9470b1905260949ca5dfcced881911db2efa
H.J.Park <oopoo1425@gmail.com>
2024-06-19 18:42:03 +0900

    FrontV content 추가

--- src/main/java/com/pcwk/ehr/servlet/FrontcontrollerV.java
+++ src/main/java/com/pcwk/ehr/servlet/FrontcontrollerV.java
@@ -14,6 +14,7 @@ import com.pcwk.ehr.cmn.ControllerV;
 import com.pcwk.ehr.cmn.JView;
 import com.pcwk.ehr.cmn.PLog;
 import com.pcwk.tvl.comment.CommentController;
+import com.pcwk.tvl.content.ContentController;
 import com.pcwk.tvl.review.ReviewController;
 import com.pcwk.tvl.user.LoginController;
 
@@ -34,6 +35,7 @@ public class FrontcontrollerV extends HttpServlet implements PLog {
     	controllerMap.put("/SEOUL_TRAVEL/user/login.do", new LoginController());
     	controllerMap.put("/SEOUL_TRAVEL/comment/comment.do", new CommentController());
     	controllerMap.put("/SEOUL_TRAVEL/review/review.do", new ReviewController());
+    	controllerMap.put("/SEOUL_TRAVEL/content/content.do", new ContentController());
     	
     }
 
