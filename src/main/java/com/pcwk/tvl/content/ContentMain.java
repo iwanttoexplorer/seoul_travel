44665f393cc9471fedf29ffc3fd5bbea4b85e20a
M.S. Jang <ms3814@naver.com>
2024-06-20 16:36:40 +0900

    테스트

--- src/main/java/com/pcwk/tvl/content/ContentMain.java
+++ src/main/java/com/pcwk/tvl/content/ContentMain.java
@@ -1,9 +1,10 @@
-package com.pcwk.tvl.content;
+package com.pcwk.tvl.contentTest;
 
 import java.util.List;
 
+import com.pcwk.ehr.cmn.ContentsSearchDTO;
 import com.pcwk.ehr.cmn.PLog;
-import com.pcwk.ehr.cmn.SearchDTO;
+
 
 public class ContentMain implements PLog {
 
@@ -13,21 +14,8 @@ public class ContentMain implements PLog {
 	public ContentMain() {
 		dao = new ContentDao();
 		
-		content01 = new ContentDTO("2638475");
+		content01 = new ContentDTO("2638475","","","","","","","","");
 		
-		log.debug("AnswerController 생성자");
-		log.debug("dao:{}", dao);
-		log.debug("answer01:{}", content01);
-	}
-	
-	public void doSave() {
-		log.debug(" doSave() ");
-		int flag =dao.doSave(content01);
-		if(1==flag) {
-			log.debug("성공 :{}",flag);
-		}else {
-			log.debug("실패 :{}",flag);
-		}
 	}
 	
 	public void doSelectOne() {
@@ -43,30 +31,30 @@ public class ContentMain implements PLog {
 	public void doRetrieve() {
 		log.debug(" doRetrieve() ");
 		
-		SearchDTO  searchVO = new SearchDTO();
-		
+		ContentsSearchDTO searchVO = new ContentsSearchDTO();
 		searchVO.setPageNo(1);
 		searchVO.setPageSize(10);
 		
+		searchVO.setSearchDiv("70");
+		searchVO.setCategoryWord("자연관광지");
+		searchVO.setgNameWord("강동구");
+		searchVO.setSearchWord("일");
+		
 		//검색구분
-		searchVO.setSearchDiv("20");
-		searchVO.setSearchWord("A05020100");
-		//searchVO.setSearchWord("");
-		//searchVO.setSearchWord("");
 		
-		List<ContentDTO> list=dao.doRetrieve(searchVO);
 		
-		int i=0;
-		for(ContentDTO vo :list) {
-			log.debug("i: {}, vo: {}", ++i, vo);
+		List<ContentDTO> list = dao.doRetrieve(searchVO);
+		
+		int i=1;
+		for(ContentDTO vo : list) {
+			log.debug("i:{}, vo:{}",i++,vo);
 		}
 	}
 	
 	public static void main(String[] args) {
 		ContentMain m =new ContentMain();
 		m.doRetrieve();
-		//m.doSave();
-		//m.doSelectOne();
+//		m.doSelectOne();
 	}
 	
 }
