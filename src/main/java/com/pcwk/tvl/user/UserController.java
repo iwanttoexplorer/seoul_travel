403bc76d4218fbc077073c975136f0961062d903
문하은🍒 <167057870+Haeun777@users.noreply.github.com>
2024-06-25 01:03:17 +0900

    Update UserController.java

--- src/main/java/com/pcwk/tvl/user/UserController.java
+++ src/main/java/com/pcwk/tvl/user/UserController.java
@@ -1,6 +1,7 @@
 package com.pcwk.tvl.user;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -10,8 +11,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.pcwk.tvl.user.UserDTO;
 import com.pcwk.ehr.cmn.ControllerV;
 import com.pcwk.ehr.cmn.JView;
+import com.pcwk.ehr.cmn.MessageVO;
 import com.pcwk.ehr.cmn.PLog;
 import com.pcwk.ehr.cmn.SearchDTO;
 import com.pcwk.ehr.cmn.StringUtil;
@@ -56,6 +59,47 @@ public class UserController extends HttpServlet implements ControllerV,PLog{
         return null;
     }
     
+
+    public JView doDeleteUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.debug("-----------------");
+        log.debug("doDeleteUser()");
+        log.debug("-----------------");
+    
+        UserDTO inVO = new UserDTO();
+        
+        String userId = StringUtil.nvl(request.getParameter("userId"), "");
+        
+        // 'userId' 값을 DTO에 설정
+        inVO.setUserId(userId);
+        log.debug("inVO:" + inVO);
+
+        // 서비스 객체를 통해 삭제 작업 수행
+        int flag = service.doDeleteUser(inVO);
+        log.debug("flag:{}", flag);
+
+        // 삭제 결과에 따라 메시지 설정
+        String message = "";
+        if (1 == flag) {
+            message = "삭제 성공";
+        } else {
+            message = "삭제 실패";
+        }
+
+        // 삭제 결과 메시지를 담기 위한 VO 객체 생성
+        MessageVO messageVO = new MessageVO();
+        messageVO.setMessageId(String.valueOf(flag));
+        messageVO.setMsgContents(message);
+        log.debug("messageVO:{}", messageVO);
+
+        Gson gson = new Gson();
+        String json = gson.toJson(userId);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);
+
+        return null;
+    }
+
     
 	@Override
 	public JView doWork(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -71,6 +115,9 @@ public class UserController extends HttpServlet implements ControllerV,PLog{
 		switch (workDiv) {
 		case "doRetrieve":
 			viewName = doRetrieve(request,response);
+			break;
+		case "doSelectOne":
+			viewName = doDeleteUser(request,response);
 			break;
 		default:
 			log.debug("작업구분을 확인 하세요. workDiv : {} ", workDiv);
